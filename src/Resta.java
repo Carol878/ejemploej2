256ed6aeee839cf7361eb7f9080f8a4ad67bf62e
Carol <nahir.torres329@comunidadunir.net>
2025-01-28 19:31:48 +0100

    Commit inicial: subo rama Master a repositorio Modifico funciones y cambio a método resta.

---
+++ src/Resta.java
@@ -0,0 +1,35 @@
+public class Resta
+{
+    // Variable para guardar acumulados
+    double resAcumulado =0;
+
+    // Creo constructor vacio, para poder llamar a la función.
+    public Resta (){
+    }
+    // Creo función resta de 2 reales
+    double restaReales (double realA, double realB)
+    {
+        double resultado = realA - realB;
+        return resultado;
+    }
+    // Creo función resta de 2 enteros
+    int restaEnteros (int enteroA, int enteroB)
+    {
+        int resultado = enteroA - enteroB;
+        return resultado;
+    }
+    // Creo función resta de 3 reales
+    double restaReales (double realA, double realB, double realC)
+    {
+        double resultado = realA - realB - realC;
+        return resultado;
+    }
+    // Resta con valor acumulados
+    double restaAcumulado (double acumulado)
+    {
+        resAcumulado = acumulado - resAcumulado;
+        return resAcumulado;
+    }
+
+
+}
