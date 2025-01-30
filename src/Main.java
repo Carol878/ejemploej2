f2b31fcc4f61e76290c43d7875a753dccb102d0a
Carol <nahir.torres329@comunidadunir.net>
2025-01-30 21:00:47 +0100

    Commit inicial: subo rama Master a repositorio Modifico funciones y cambio a método resta. Creo bucle do-while. Añado funciones 2 switches. Pido datos al usuario y me los guardo (scanner). Realizo las operaciones que pide el usuario y las saco por pantalla.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,69 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
-        //Llamo al constructor (objeto tipo resta = varResta)
-        Resta varResta  = new Resta();
+    public static void main(String[] args) {
+        String opciones;
+        Resta resta = new Resta();
+        do {
+            System.out.println("¿Qué tipo de operación quieres realizar?");
+            System.out.println("1 Suma, 2 Resta, 3 Multiplicar, 4 Dividir, 5 Potencia, 6 Raiz cuadrada, 7 Salir");
+            Scanner sc = new Scanner(System.in);
 
-        //Llamo a la funcion y compruebo que funciona
-        int prueba1 = varResta.restaEnteros(2, 4);
-        System.out.println(prueba1);
-        double prueba2 = varResta.restaReales(3.5, 1);
-        System.out.println(prueba2);
-        double prueba3 = varResta.restaAcumulado(5);
-        System.out.println(prueba3);
-        prueba3 = varResta.restaAcumulado(2);
-        System.out.println(prueba3);
-        prueba3 = varResta.restaAcumulado(7);
-        System.out.println(prueba3);
-        prueba3 = varResta.restaAcumulado(1.5);
-        System.out.println(prueba3);
+            opciones = sc.next();
+            switch (opciones) {
+                case "1":
+                    //TODO: Suma;
+                    break;
+                case "2":
+                    System.out.println("¿Qué tipo de operación quieres realizar?");
+                    System.out.println("1 Resta de dos reales, 2 Resta de dos enteros, 3 Resta de tres reales, 4 Resta acumulados");
+                    opciones = sc.next();
+                    switch (opciones) {
+                        case "1":
+                            System.out.println("Dame 2 números reales");
+                            double numero1 = sc.nextDouble();
+                            double numero2 = sc.nextDouble();
+                            System.out.println(resta.restaReales(numero1, numero2));
+                            break;
+                        case "2":
+                            System.out.println("Dame 2 números enteros");
+                            int numero3 = sc.nextInt();
+                            int numero4 = sc.nextInt();
+                            System.out.println(resta.restaEnteros(numero3, numero4));
+                            break;
+                        case "3":
+                            System.out.println("Dame 3 números reales");
+                            double numero5 = sc.nextDouble();
+                            double numero6 = sc.nextDouble();
+                            double numero7 = sc.nextDouble();
+                            System.out.println(resta.restaReales(numero5, numero6, numero7));
+                            break;
+                        case "4":
+                            System.out.println("Dame 1 número real");
+                            double numero8 = sc.nextDouble();
+                            System.out.println(resta.restaAcumulado(numero8));
+                            break;
+                        default:
+                            System.out.println("Opcion no valida");
+                    }
+                case "3":
+                    // TODO: Multiplicar;
+                    break;
+                case "4":
+                    // TODO: Dividir;
+                    break;
+                case "5":
+                    // TODO: Potencia;
+                    break;
+                case "6":
+                    // TODO: Raiz cuadrada;
+                    break;
+                case "7":
+                    System.out.println("Salir");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
+        while (!opciones.equals("7"));
     }
-
 }
